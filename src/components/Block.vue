e5359c47f8fc22c1dbb22286aab1d74c71661fce
rbxMatrix <rbx.matrix@gmail.com>
2025-04-28 17:01:23 +0200

    Initial commit

---
+++ src/components/Block.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="block" v-if=" showBlock">
+    click me
+  </div>
+</template>
+
+<script>
+export default {
+    props:['delay'],
+    data(){
+        return{
+            showBlock: false
+        }
+    },
+    mounted(){
+        console.log('componet mounted')        
+        setTimeout(()=>{
+            this.showBlock= true
+            console.log(this.delay);
+
+        }, this.delay)
+    },
+    updated(){
+        console.log('componet updated');
+    },
+    unmounted(){
+        console.log('componet updated');
+    }
+}
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;        
+    }
+</style>
